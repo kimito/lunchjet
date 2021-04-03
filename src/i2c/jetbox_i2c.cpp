d6eb356e059059a806fcd2f7e81924358763c4a7
kimitok <1219kk@gmail.com>
2021-04-04 07:36:16 +0900

    add i2c transaction for write

--- src/i2c/jetbox_i2c.cpp
+++ src/i2c/jetbox_i2c.cpp
@@ -13,13 +13,49 @@
 #include <cassert>
 
 #include "debug_log.h"
+#include "string_utils.h"
 
 
 namespace jetbox {
 
+i2c_msg I2CWriteCommnand::msg() {
+    return {device_address, 0, static_cast<uint16_t>(data.size()), data.data()};
+}
+
+
+I2CWriteTransaction& I2CWriteTransaction::add_write(uint8_t register_address, uint8_t value) {
+    commands.push_back(I2CWriteCommnand(device.device_address(), register_address, value));
+
+    return *this;
+}
+
+int I2CWriteTransaction::send()
+{
+    std::vector<i2c_msg> msgs;
+    for(auto &command : commands) {
+        msgs.push_back(command.msg());
+    }
+
+    struct i2c_rdwr_ioctl_data data = {msgs.data(), static_cast<uint32_t>(msgs.size())};
+    std::cout << "buf size: " << data.msgs[0].len << std::endl;
+    std::cout << "register address:" << hex2str(data.msgs[0].buf[0])  << std::endl;
+    std::cout << "register value:" << hex2str(data.msgs[0].buf[1]) << std::endl;
+
+    int ret;
+    errno = 0;
+    if((ret = ioctl(device.fd(), I2C_RDWR, &data)) < 0) {
+        debug_err("ioctl() failed: device:%02x reason:%s",
+            device.device_address(),
+            strerror(errno));
+    }
+
+    return ret;
+}
+
+
 I2CDevice::I2CDevice(uint8_t adapter_number, uint8_t device_address)
     :adapter_number(adapter_number),
-    device_address(device_address),
+    _device_address(device_address),
     adapter_fd(0)
 {
     std::string dev_name = "/dev/i2c-" + std::to_string(adapter_number);
@@ -35,15 +71,15 @@ int I2CDevice::read(uint8_t register_address, uint8_t &value)
 {
     int ret;
     std::vector<i2c_msg> msgs = {
-        {device_address, 0, 1, &register_address},
-        {device_address, I2C_M_RD, 1, &value}
+        {_device_address, 0, 1, &register_address},
+        {_device_address, I2C_M_RD, 1, &value}
     };
     struct i2c_rdwr_ioctl_data data = {msgs.data(), static_cast<uint32_t>(msgs.size())};
 
     errno = 0;
     if((ret = ioctl(adapter_fd, I2C_RDWR, &data)) < 0) {
         debug_err("ioctl() failed: device:%02x register:%02x reason:%s",
-            device_address,
+            _device_address,
             register_address,
             strerror(errno));
     }
@@ -53,22 +89,7 @@ int I2CDevice::read(uint8_t register_address, uint8_t &value)
 
 int I2CDevice::write(uint8_t register_address, uint8_t value)
 {
-    std::vector<uint8_t> msg = {register_address, value};
-    std::vector<i2c_msg> msgs = {
-        {device_address, 0, static_cast<uint16_t>(msg.size()), msg.data()}
-    };
-    struct i2c_rdwr_ioctl_data data = {msgs.data(), static_cast<uint32_t>(msgs.size())};
-
-    int ret;
-    errno = 0;
-    if((ret = ioctl(adapter_fd, I2C_RDWR, &data)) < 0) {
-        debug_err("ioctl() failed: device:%02x register:%02x reason:%s",
-            device_address,
-            register_address,
-            strerror(errno));
-    }
-
-    return ret;
+    return transaction().add_write(register_address, value).send();
 }
 
 I2CDevice::~I2CDevice()
--- src/i2c/jetbox_i2c.h
+++ src/i2c/jetbox_i2c.h
@@ -1,8 +1,51 @@
 #pragma once
 
 #include <stdint.h>
+#include <vector>
+
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
 
 namespace jetbox {
+
+class I2CDevice;
+
+class I2CWriteCommnand {
+    public:
+    I2CWriteCommnand() = delete;
+    I2CWriteCommnand(uint8_t device_address, uint8_t register_address, uint8_t value)
+     :device_address(device_address),
+      data({register_address, value}){}
+    ~I2CWriteCommnand() = default;
+
+    i2c_msg msg();
+
+    private:
+    uint8_t device_address;
+    std::vector<uint8_t> data;
+};
+
+class I2CWriteTransaction{
+    public:
+    I2CWriteTransaction(I2CDevice &device) : device(device){};
+    ~I2CWriteTransaction() = default;
+
+    /**
+     * add a write commands to the transaction
+     * @param[in] register_address register address
+     * @param[in] register_value register value
+     */
+    I2CWriteTransaction& add_write(uint8_t register_address, uint8_t value);
+
+    /**
+     * send the transaction to the device
+     */
+    int send();
+
+    private:
+    I2CDevice &device;
+    std::vector<I2CWriteCommnand> commands;
+};
 
 class I2CDevice {
     public:
@@ -13,9 +56,16 @@ class I2CDevice {
     int read(uint8_t register_address, uint8_t &value);
     int write(uint8_t register_address, uint8_t value);
 
+    I2CWriteTransaction transaction() {
+        return I2CWriteTransaction(*this);
+    };
+
+    int fd(){ return adapter_fd; }
+    uint8_t device_address() { return _device_address; }
+
     private:
     uint8_t adapter_number;
-    uint8_t device_address;
+    uint8_t _device_address;
     int adapter_fd;
 };
 
