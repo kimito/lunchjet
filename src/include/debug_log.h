8cd8174813b7bbd7549a31392cdf24e500ab9a08
kimitok <1219kk@gmail.com>
2021-04-04 05:44:00 +0900

    shorten debug log message

--- src/include/debug_log.h
+++ src/include/debug_log.h
@@ -2,11 +2,11 @@
 
 #include <syslog.h>
 
-#define debug_debug( message, ... ) (syslog(LOG_DEBUG, "[%s]" message, __PRETTY_FUNCTION__, __VA_ARGS__))
-#define debug_info( message, ... ) (syslog(LOG_INFO, "[%s]" message, __PRETTY_FUNCTION__, __VA_ARGS__))
-#define debug_notice( message, ... ) (syslog(LOG_NOTICE, "[%s]" message, __PRETTY_FUNCTION__, __VA_ARGS__))
-#define debug_warning( message, ... ) (syslog(LOG_WARNING, "[%s]" message, __PRETTY_FUNCTION__, __VA_ARGS__))
-#define debug_err( message, ... ) (syslog(LOG_ERR, "[%s]" message, __PRETTY_FUNCTION__, __VA_ARGS__))
-#define debug_crit( message, ... ) (syslog(LOG_CRIT, "[%s]" message, __PRETTY_FUNCTION__, __VA_ARGS__))
-#define debug_alert( message, ... ) (syslog(LOG_ALERT, "[%s]" message, __PRETTY_FUNCTION__, __VA_ARGS__))
-#define debug_emerg( message, ... ) (syslog(LOG_EMERG, "[%s]" message, __PRETTY_FUNCTION__, __VA_ARGS__))
+#define debug_debug( message, ... ) (syslog(LOG_DEBUG, "[%s]" message, __func__, __VA_ARGS__))
+#define debug_info( message, ... ) (syslog(LOG_INFO, "[%s]" message, __func__, __VA_ARGS__))
+#define debug_notice( message, ... ) (syslog(LOG_NOTICE, "[%s]" message, __func__, __VA_ARGS__))
+#define debug_warning( message, ... ) (syslog(LOG_WARNING, "[%s]" message, __func__, __VA_ARGS__))
+#define debug_err( message, ... ) (syslog(LOG_ERR, "[%s]" message, __func__, __VA_ARGS__))
+#define debug_crit( message, ... ) (syslog(LOG_CRIT, "[%s]" message, __func__, __VA_ARGS__))
+#define debug_alert( message, ... ) (syslog(LOG_ALERT, "[%s]" message, __func__, __VA_ARGS__))
+#define debug_emerg( message, ... ) (syslog(LOG_EMERG, "[%s]" message, __func__, __VA_ARGS__))
