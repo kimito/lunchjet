b25c52c63f674add9a219650ed913b3a4cf60bbe
kimitok <1219kk@gmail.com>
2021-04-04 15:14:34 +0900

    modify PCA8562 test

--- test/pca9685_test.cpp
+++ test/pca9685_test.cpp
@@ -7,25 +7,54 @@ using namespace jetbox;
 
 int main(int argc, const char *argv[])
 {
-    PCA9685 driver(17.25 * 1000);
+    PCA9685 driver(17.5 * 1000);
 
     driver.set_pulse(0, 1.5 * 1000);
     driver.set_pulse(1, 1.5 * 1000);
 
-//    driver.start();
+    driver.start();
+    std::this_thread::sleep_for(std::chrono::seconds(2));
 
-    std::this_thread::sleep_for(std::chrono::seconds(3));
-
+    //servo motor control
+    //turn left
     driver.set_pulse(0, 1.1 * 1000);
-    driver.set_pulse(1, 1.1 * 1000);
-
-    std::this_thread::sleep_for(std::chrono::seconds(3));
-
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    
+    //neutral
+    driver.set_pulse(0, 1.5 * 1000);
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    
+    //turn right
     driver.set_pulse(0, 1.9 * 1000);
-    driver.set_pulse(1, 1.9 * 1000);
+    std::this_thread::sleep_for(std::chrono::seconds(2));
 
-//    std::this_thread::sleep_for(std::chrono::seconds(3));
-//    driver.reset();
+    //neutral
+    driver.set_pulse(0, 1.5 * 1000);
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
+
+    //drive mortor control
+    //move backward
+    driver.set_pulse(1, 1.9 * 1000);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    driver.set_pulse(1, 1.5 * 1000);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    driver.set_pulse(1, 1.6 * 1000);
+
+    //neutral
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    driver.set_pulse(1, 1.5 * 1000);
+
+    //move forward
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    driver.set_pulse(1, 1.45 * 1000);
+
+    //neutral
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    driver.set_pulse(1, 1.5 * 1000);
+
+    // std::this_thread::sleep_for(std::chrono::seconds(3));
+    // driver.reset();
 
     return 0;
 }
