4212b1665899e64e0aeb9cf00b2b7d15672345fe
kimitok <1219kk@gmail.com>
2021-04-06 15:36:58 +0900

    delete #if for debug

--- test/rc_car_test.cpp
+++ test/rc_car_test.cpp
@@ -9,13 +9,13 @@ using namespace jetbox;
 int main(int argc, const char *argv[])
 {
     RCCar car;
-#if 0
+
     //swing the steering
     for(float s = 0.f; s <= 3.f; s+=0.05f) {
         car.steer(std::sin(2*M_PI*s));
         std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
-#elif 1
+
     //stop and go
     for(float s = 0.f; s <= 2.f; s+=0.01f) {
         auto speed = 0.2/*max speed cap*/ * ((-std::cos(2*M_PI*s)+1)/2);
@@ -24,5 +24,4 @@ int main(int argc, const char *argv[])
     }
 
     return 0;
-#endif
 }
