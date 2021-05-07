cd73abd7b36327d019b51b5a8371f608fa8f349e
kimitok <1219kk@gmail.com>
2021-05-08 05:50:53 +0900

    Revert "add VideoInputDevice class"
    This reverts commit 47bad5037d479da27508b4759794959a409009c8.

--- test/video_device_test.cpp
+++ test/video_device_test.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
+#include <opencv2/videoio.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
 
-#include "video_input_device.h"
+#include "performance_count.h"
 
-using namespace lunchjet;
 
 int main(int argc, const char *argv[])
 {
     int device_num = 2;
-    VideoInputDevice device(device_num);
+    cv::VideoCapture video(device_num);
+
+    if( !video.isOpened() ) {
+        std::cerr << "cannot open video device: " << device_num << std::endl;
+        return -1;
+    }
 
     cv::Mat image;
 
     for(int i = 0; i < 10; ++i)
     {
-        device.save_image("image" + std::to_string(i)  + ".jpg",
-        [](auto file_name, auto result){std::cout << "complete: " << file_name << std::endl;});
+        auto start = time_now();
+        if( !video.read(image) ) {
+            std::cerr << "cannot grab any image" << std::endl;
+            return -1;
+        }
+        std::cout << "time to read: " << duration_ms_from(start) << std::endl;
     }
+
+    cv::imwrite("image.jpg", image);
 
     return 0;
 }
