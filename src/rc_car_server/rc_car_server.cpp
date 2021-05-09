f14e1ca07f2e17dc89ea7bd503cf8aa9e3ad700a
kimitok <1219kk@gmail.com>
2021-05-09 17:13:52 +0900

    move implementations

--- src/rc_car_server/rc_car_server.cpp
+++ src/rc_car_server/rc_car_server.cpp
@@ -1,1 +1,44 @@
 #include "rc_car_server.h"
+
+namespace lunchjet {
+
+RCCarServer::RCCarServer(std::atomic<bool> &stop_controller_thread)
+ : controller("/dev/input/event2", *this, stop_controller_thread)
+{
+}
+
+int RCCarServer::start() {
+    return controller.listen();
+}
+
+void RCCarServer::on_connect(){
+    debug_notice("controller connected");
+};
+
+void RCCarServer::on_change_steering(float value) {
+    driver.steer(value);
+}
+
+void RCCarServer::on_change_accel(float value) {
+    if(is_going_back) {
+        driver.back(value * 0.3f);
+    }
+    else {
+        driver.go(value * 0.2f);
+    }
+}
+
+void RCCarServer::on_change_back(int value) {
+    if(value) {
+        is_going_back = true;
+    }
+    else {
+        is_going_back = false;
+    }
+}
+
+void RCCarServer::on_close() {
+    debug_notice("controller disconnected");
+}
+
+} //namespace lunchjet
--- src/rc_car_server/rc_car_server.h
+++ src/rc_car_server/rc_car_server.h
@@ -10,49 +10,30 @@
 namespace lunchjet
 {
 
+
 class RCCarServer : public RCCarControllerListener {
     public:
-    RCCarServer(std::atomic<bool> &stop_controller_thread) : controller("/dev/input/event2", *this, stop_controller_thread){}
+    RCCarServer() = delete;
+    RCCarServer(std::atomic<bool> &stop_controller_thread);
     ~RCCarServer() = default;
 
-    int start() {
-        return controller.listen();
-    }
+    /**
+     * start processing
+     */
+    int start();
 
-    void on_connect(){
-        debug_notice("controller connected");
-    };
-
-    void on_change_steering(float value) override {
-        driver.steer(value);
-    }
-
-    void on_change_accel(float value) override {
-        if(is_going_back) {
-            driver.back(value * 0.3f);
-        }
-        else {
-            driver.go(value * 0.2f);
-        }
-    }
-
-    void on_change_back(int value) override {
-        if(value) {
-            is_going_back = true;
-        }
-        else {
-            is_going_back = false;
-        }
-    }
-
-    void on_close() {
-        debug_notice("controller disconnected");
-    }
+    //member functions for RCCarControllerListener
+    void on_connect() override;
+    void on_change_steering(float value) override;
+    void on_change_accel(float value) override;
+    void on_change_back(int value) override;
+    void on_close() override;
 
     private:
     RCCarDriver driver;
     RCCarController controller;
     bool is_going_back = false;
 };
-    
+
+ 
 } //namespace lunchjet
