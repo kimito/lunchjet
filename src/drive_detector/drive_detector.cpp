3063af1001c22d4f1c0394d25e5a287f08519cff
kimitok <1219kk@gmail.com>
2021-06-23 05:21:55 +0900

    update DriveDetector (still under construction)

--- src/drive_detector/drive_detector.cpp
+++ src/drive_detector/drive_detector.cpp
@@ -1,11 +1,116 @@
 #include "drive_detector.h"
 
+#include <fstream>
+#include <memory>
+#include <numeric>
+
+#include <cuda_runtime_api.h>
+
+#include <debug_log.h>
+
 
 namespace lunchjet {
 
+static size_t getMemorySize(const nvinfer1::Dims& dims, const int32_t elem_size)
+{
+    return std::accumulate(dims.d, dims.d + dims.nbDims, 1, std::multiplies<int64_t>()) * elem_size;
+}
+
+class Logger : public nvinfer1::ILogger           
+ {
+     void log(Severity severity, const char* msg) override
+     {
+         // suppress info-level messages
+         if (/*severity < Severity::kINFO*/ true)
+            // debug_debug("TensorRT log: %d %s", severity ,msg);
+            std::cout << static_cast<int>(severity) << ":" << msg << std::endl;
+     }
+};
+static Logger logger;
+static DriveParameter value_on_error = {0.f, 0.f};
+static const int IMAGE_HEIGHT = 240;
+static const int IMAGE_WIDTH = 360;
+
+DriveDetector::DriveDetector(const std::string &model_file_path)
+:engine_file_path(model_file_path),
+ engine(nullptr)
+{
+    std::ifstream engine_file(engine_file_path, std::ios::binary);
+    if (engine_file.fail())
+    {
+        return;
+    }
+
+    //getting size of engine file
+    engine_file.seekg(0, std::ifstream::end);
+    auto fsize = engine_file.tellg();
+    engine_file.seekg(0, std::ifstream::beg);
+
+    std::vector<char> engine_data(fsize);
+    engine_file.read(engine_data.data(), fsize);
+
+    //get engine
+    unique_ptr_for_nv<nvinfer1::IRuntime> runtime{nvinfer1::createInferRuntime(logger)};
+    engine.reset(runtime->deserializeCudaEngine(engine_data.data(), fsize, nullptr));
+    if(engine == nullptr) {
+        std::cout << "deserializeCudaEngine() failed" << std::endl;
+    }
+}
+
+
 DriveParameter DriveDetector::detect(const cv::Mat &image)
 {
-    return {0.f, 0.f};
+    unique_ptr_for_nv<nvinfer1::IExecutionContext> context{engine->createExecutionContext()};
+    if(!context) {
+        debug_err("createExecutionContext() failed");
+        return value_on_error;
+    }
+
+    //configure input tensor
+    std::string input_name("input:0");
+    auto input_index = engine->getBindingIndex(input_name.c_str());
+    std::cout << "input index: " << input_index << std::endl;
+    if(input_index == -1) {
+        debug_err("getBindingIndex() for input failed: %s", input_name.c_str());
+        return value_on_error;
+    }
+    auto input_dims = nvinfer1::Dims4{1, IMAGE_HEIGHT, IMAGE_WIDTH, 3 /* channels */};
+    auto input_mem_size = getMemorySize(input_dims, sizeof(float));
+    if(context->setBindingDimensions(input_index, input_dims) == false) {
+        debug_err("setBindingDimensions() for input failed");
+        return value_on_error;
+    }
+
+    //configure output tensor
+    std::string output_name("output");
+    auto output_index = engine->getBindingIndex(output_name.c_str());
+    std::cout << "output index: " << output_index << std::endl;
+    if(output_index == -1) {
+        debug_err("getBindingIndex() for output failed: %s", input_name.c_str());
+        return value_on_error;
+    }
+    auto output_dims = context->getBindingDimensions(output_index);
+    debug_debug("output dims:");
+    for(int i=0;i<output_dims.nbDims;++i) {
+        debug_debug("  dim %d: %d", i, output_dims.d[i]);
+    }
+    auto output_mem_size = getMemorySize(output_dims, sizeof(float));
+
+    // Allocate CUDA memory for input and output bindings
+    void* input_mem{nullptr};
+    if (cudaMalloc(&input_mem, input_mem_size) != cudaSuccess)
+    {
+        debug_err("input cuda memory allocation failed, size = %ld bytes", input_mem_size);
+        return value_on_error;
+    }
+    void* output_mem{nullptr};
+    if (cudaMalloc(&output_mem, output_mem_size) != cudaSuccess)
+    {
+        debug_err("ERROR: output cuda memory allocation failed, size = %ld bytes", output_mem_size);
+        return value_on_error;
+    }
+
+    return value_on_error;
 }
 
 } //namespace lunchjet
--- src/drive_detector/drive_detector.h
+++ src/drive_detector/drive_detector.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <string>
+#include <memory>
 
-#include <opencv2/opencv.hpp>
+#include "opencv2/opencv.hpp"
+#include "NvInfer.h"
 
 
 namespace lunchjet {
@@ -15,14 +17,25 @@ struct DriveParameter {
 class DriveDetector {
     public:
 
-    DriveDetector(const std::string &model_file_path)
-        : model_file_path(model_file_path) {}
+    DriveDetector(const std::string &model_file_path);
     ~DriveDetector() = default;
 
     DriveParameter detect(const cv::Mat &image);
 
     private:
-    std::string model_file_path;
+
+    struct InferDeleter
+    {
+        template <typename T>
+        void operator()(T* obj) const
+        {
+            if (obj) obj->destroy();
+        }
+    };
+    template <typename T> using unique_ptr_for_nv = std::unique_ptr<T, InferDeleter>;
+
+    std::string engine_file_path;
+    unique_ptr_for_nv<nvinfer1::ICudaEngine> engine;
 };
 
 } //namespace lunchjet
